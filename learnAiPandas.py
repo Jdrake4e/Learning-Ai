371707cf4f4f210392af81614e650db9b4afe912
Jdrake4e <john4e.hohman@gmail.com>
2018-09-24 20:33:13 -0400

    Add files via upload

---
+++ learnAiPandas.py
@@ -0,0 +1,6 @@
+# import Pandas
+import pandas as pd
+
+# Data frame collection of column with same index, a tabular structure
+s1 = pd.Series(data=[1, 2, 3, 4], index=['a', 'b', 'c', 'd'])
+print(s1)
